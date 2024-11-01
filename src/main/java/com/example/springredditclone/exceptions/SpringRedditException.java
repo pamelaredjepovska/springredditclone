9c69f563c6b397f42e7e64c288d17de680a834fc
Pamela Redjepovska <pamelaredjepovska@gmail.com>
2024-11-01 18:33:47 +0100

    Class for custom excpetion handling

---
+++ src/main/java/com/example/springredditclone/exceptions/SpringRedditException.java
@@ -0,0 +1,12 @@
+package com.example.springredditclone.exceptions;
+
+// Class for custom exception handling
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String exMessage, Exception exception) {
+        super(exMessage, exception);
+    }
+
+    public SpringRedditException(String exMessage) {
+        super(exMessage);
+    }
+}
