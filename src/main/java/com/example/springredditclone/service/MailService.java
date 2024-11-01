de3e0c106403c59a1132c97e34193fa498abbe20
Pamela Redjepovska <pamelaredjepovska@gmail.com>
2024-11-01 18:31:51 +0100

    Sent succesfull activation email to the user

---
+++ src/main/java/com/example/springredditclone/service/MailService.java
@@ -0,0 +1,43 @@
+package com.example.springredditclone.service;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import com.example.springredditclone.exceptions.SpringRedditException;
+import com.example.springredditclone.model.NotificationEmail;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+    private final JavaMailSender mailSender;
+    // private final MailContentBuilder mailContentBuilder;
+
+    @Async
+    void sendMail(NotificationEmail notificationEmail) {
+        // Objects to prepare the email message and set the information
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springreddit@email.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(notificationEmail.getBody());
+        };
+        try {
+            // Try to send the email
+            // If an exception occurs, log the error and throw an exception
+            mailSender.send(messagePreparator);
+            log.info("Activation email sent!!");
+        } catch (MailException e) {
+            log.error("Exception occurred when sending mail", e);
+            throw new SpringRedditException("Exception occurred when sending mail to " + notificationEmail.getRecipient(), e);
+        }
+    }
+}
