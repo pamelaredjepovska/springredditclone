940506cad8ba007deb002f71c8b2a920c60b340e
Pamela Redjepovska <pamelaredjepovska@gmail.com>
2024-11-02 16:23:05 +0100

    Update port in email message

--- src/main/java/com/example/springredditclone/service/AuthService.java
+++ src/main/java/com/example/springredditclone/service/AuthService.java
@@ -43,7 +43,7 @@ public class AuthService {
         mailService.sendMail(new NotificationEmail("Please activate your account",
                 user.getEmail(), "Thank you for signing up to Spring Reddit, " +
                 "please click on the link below to activate your account : " +
-                "http://localhost:3306/api/auth/accountVerification/" + token));
+                "http://localhost:8080/api/auth/accountVerification/" + token));
     }
 
     private String generateVerificationToken(User user) {
