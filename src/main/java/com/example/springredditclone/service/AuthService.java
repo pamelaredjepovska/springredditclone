50e7b3073a8381c8cec432f03c9059f7794a449d
Pamela Redjepovska <pamelaredjepovska@gmail.com>
2024-11-06 20:05:26 +0100

    Service method for login, DTO for authentication response, comments update

--- src/main/java/com/example/springredditclone/service/AuthService.java
+++ src/main/java/com/example/springredditclone/service/AuthService.java
@@ -4,9 +4,15 @@ import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.springredditclone.dto.AuthenticationResponse;
+import com.example.springredditclone.dto.LoginRequest;
 import com.example.springredditclone.dto.RegisterRequest;
 import com.example.springredditclone.exceptions.SpringRedditException;
 import com.example.springredditclone.model.NotificationEmail;
@@ -14,10 +20,13 @@ import com.example.springredditclone.model.User;
 import com.example.springredditclone.model.VerificationToken;
 import com.example.springredditclone.repository.UserRepository;
 import com.example.springredditclone.repository.VerificationTokenRepository;
+import com.example.springredditclone.security.JwtProvider;
 
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 
+
+// Service class for managing user authentication tasks
 @Service
 @Transactional
 @AllArgsConstructor
@@ -26,7 +35,10 @@ public class AuthService {
     private final UserRepository userRepository;
     private final VerificationTokenRepository verificationTokenRepository;
     private final MailService mailService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtProvider jwtProvider;
     
+    // Register the user
     public void signup(RegisterRequest registerRequest) {
         // Create new User object with the specified information
         User user = new User();
@@ -42,12 +54,14 @@ public class AuthService {
         // Generate verification token
         String token = generateVerificationToken(user);
 
+        // Send an activation email to the user
         mailService.sendMail(new NotificationEmail("Please activate your account",
                 user.getEmail(), "Thank you for signing up to Spring Reddit, " +
                 "please click on the link below to activate your account : " +
                 "http://localhost:8080/api/auth/accountVerification/" + token));
     }
 
+    // Generate the user's verification token
     private String generateVerificationToken(User user) {
         // Generate random UUID as a verification token
         String token = UUID.randomUUID().toString();
@@ -61,12 +75,19 @@ public class AuthService {
         return token;
     }
 
+
+    // Activate the user's account
     public void verifyAccount(String token) {
-        // Find the verification token and activate the user
+        // Find the verification token
         Optional<VerificationToken> verificationToken = verificationTokenRepository.findByToken(token);
+
+        // If it exists, find the user and activate the account.
+        // Otherwise, throw an exception
         fetchUserAndActivate(verificationToken.orElseThrow(() -> new SpringRedditException("Invalid Token")));
-            }
+    }
         
+
+    // Activate the user's account associated with a given verification token
     private void fetchUserAndActivate(VerificationToken verificationToken) {
         // Get the username from the token
         String username = verificationToken.getUser().getUsername();
@@ -77,5 +98,25 @@ public class AuthService {
         // Activate the user account and save it to DB
         user.setActivated(true);
         userRepository.save(user);
+    }
+
+    // Login the user
+    public AuthenticationResponse login(LoginRequest loginRequest) {
+        // Authenticate the user's credentials
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        // Mark the user as authenticated within the session
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+
+        // Generate a JWT token using the user's details
+        String token = jwtProvider.generateToken(authenticate);
+        
+        // Return an object with the token, expiration time and username
+        return AuthenticationResponse.builder()
+                .authenticationToken(token)
+                // .refreshToken(refreshTokenService.generateRefreshToken().getToken())
+                .expiresAt(Instant.now().plusMillis(jwtProvider.getJwtExpirationInMillis()))
+                .username(loginRequest.getUsername())
+                .build();
     }
 }
